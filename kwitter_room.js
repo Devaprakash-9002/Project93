1e83efe6db0e3861a77cad420a814f182a84a8f5
Devaprakash-9002 <79247856+Devaprakash-9002@users.noreply.github.com>
2021-06-08 19:34:29 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,21 @@
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyASkzlTbPjzIEHdNmm9PYjCR2s7T5_dvNI",
+    authDomain: "letschat-c8e3c.firebaseapp.com",
+    projectId: "letschat-c8e3c",
+    storageBucket: "letschat-c8e3c.appspot.com",
+    messagingSenderId: "504105466206",
+    appId: "1:504105466206:web:4dda31b62a2cb06fd3f7cf",
+    measurementId: "G-ZKHQH4F0NQ"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
